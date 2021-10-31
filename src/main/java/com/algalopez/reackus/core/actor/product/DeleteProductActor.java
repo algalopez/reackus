ed7d56c0f7bec5b9b3bc2b46dd3f01fb3b4320ba
algalopez <algalopez85@gmail.com>
2021-10-31 16:50:37 +0100

    Issue-0: Update pipeline

--- src/main/java/com/algalopez/reackus/core/actor/product/DeleteProductActor.java
+++ src/main/java/com/algalopez/reackus/core/actor/product/DeleteProductActor.java
@@ -1,7 +1,6 @@
 package com.algalopez.reackus.core.actor.product;
 
 import com.algalopez.reackus.api.common.BaseInteractor;
-import com.algalopez.reackus.core.model.Product;
 import io.smallrye.mutiny.Uni;
 
 import javax.enterprise.context.ApplicationScoped;
